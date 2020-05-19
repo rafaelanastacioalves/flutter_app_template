a252a861398064c7ca15b43ae3ad18c89406e7ec
rafaelanastacio <rafaelanastacioalves@gmail.com>
2020-05-19 05:53:59 -0300

    refactor: Extract variable names etc

--- lib/database/DAO.dart
+++ lib/database/DAO.dart
@@ -3,30 +3,34 @@ import 'package:flutter_app_template/models/MainEntity.dart';
 import 'package:sqflite/sqflite.dart';
 
 class DAO {
+  final _main_entitiy_table_name = 'main_entity';
+  final _id = 'id';
+  final _title = 'title';
+
   Future<int> save(MainEntity mainEntity) async {
     final Database db = await getDatabase();
     Map<String, dynamic> mainEntityMap = _toMap(mainEntity);
-    return db.insert('main_entity', mainEntityMap);
+    return db.insert(_main_entitiy_table_name, mainEntityMap);
   }
 
   Future<List<MainEntity>> findAll() async {
     final Database db = await getDatabase();
     List<Map<String, dynamic>> mainEntityListMap =
-        await db.query('main_entity');
+        await db.query(_main_entitiy_table_name);
     List<MainEntity> mainEntityList = _toList(mainEntityListMap);
     return mainEntityList;
   }
 
   Map<String, dynamic> _toMap(MainEntity mainEntity) {
     var map = Map<String, dynamic>();
-    map['title'] = mainEntity.title;
+    map[_title] = mainEntity.title;
     return map;
   }
 
   List<MainEntity> _toList(List<Map<String, dynamic>> mainEntityListMap) {
     List<MainEntity> mainEntityList = List<MainEntity>();
     mainEntityListMap.forEach((mapItem) {
-      MainEntity mainEntity = MainEntity(id: mapItem['id'],title: mapItem['title']);
+      MainEntity mainEntity = MainEntity(id: mapItem[_id],title: mapItem[_title]);
       mainEntityList.add(mainEntity);
     });
     return mainEntityList;
--- lib/database/database.dart
+++ lib/database/database.dart
@@ -2,11 +2,13 @@ import 'package:path/path.dart';
 import 'package:sqflite/sqflite.dart';
 
 Future<Database> getDatabase() async {
-  final String path = join(await getDatabasesPath(), 'app_name.db');
+
+  final _dbName = 'app_name.db';
+  final String path = join(await getDatabasesPath(), _dbName);
   return openDatabase(
     path,
     onCreate: (db, version) {
-      db.execute("CREATE TABLE main_entity("
+      db.execute("CREATE IF NOT EXISTS TABLE main_entity("
           "id INTEGER PRIMARY KEY, "
           "title TEXT"
           ")");
